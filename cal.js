1c9f01c1a0195f34022b99a93289a8feb8684d2f
SUHAIR SUBAIR <suhairsubair0@gmail.com>
2025-12-07 16:54:14 +0530

    Update cal.js

--- cal.js
+++ cal.js
@@ -4,6 +4,13 @@ function clickedi(value)
    const display= document.getElementById("screen")
    const lastChar=display.value.slice(-1)
    const operators=['+','-','*','/']
+   if (value==="."){
+      let parts=display.value.split(/[\]+\-\*\]\);
+      let lastnumber=parts[parts.length-1];
+      if (lastnumber.includes.("."){
+         return;
+      }
+   }
    if (operators.includes(value)&&operators.includes(lastChar))
     {
     return;
